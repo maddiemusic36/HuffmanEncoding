8d8862935a5fe1aed9e00ecc69919f06d89022c5
maddiemusic36 <126003860+maddiemusic36@users.noreply.github.com>
2023-10-30 16:53:40 -0700

    Create Huffman.java

---
+++ Huffman.java
@@ -0,0 +1,30 @@
+public class Huffman<K,V> {
+    
+    private int size;
+    private Node<K,V> root;
+
+    Huffman() {
+        this.size = 0;
+        this.root = null;
+    }
+
+    ///// function to add value to tree \\\\\
+    
+    ///// function to create the encoding \\\\\
+
+    ///// function to print the tree \\\\\
+}
+
+class Node<K,V> {
+    String key;
+    int val;
+    Node<K,V> left;
+    Node<K,V> right;
+
+    Node(String key, int value) {
+        this.key = key;
+        this.val = value;
+        this.left = null;
+        this.right = null;
+    }
+}
