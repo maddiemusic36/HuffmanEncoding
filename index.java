ddf1ae051914e37bfbc0f1e0c03c0dcd56951aab
maddiemusic36 <126003860+maddiemusic36@users.noreply.github.com>
2023-10-30 16:53:12 -0700

    Create index.java

---
+++ index.java
@@ -0,0 +1,55 @@
+/**
+ * This project ____
+ * Authors: Madeline DeLeon, Billy Dolny, Savannah Rabasa, Julia Ryan
+*/
+
+// for file reading
+import java.io.File;
+
+public class index<K, V> {
+
+
+    public void organizeInput(String filename) {
+    /**
+     * This function reads in the text from the given file, counts the
+     * frequency of each character, and creates a priority queue of said
+     * frequencies.
+     * Args: filename, which is a string representing the name of a file
+     * Returns: ______
+     */
+
+    }
+
+    public int inputSize(String fileString) {
+    /**
+     * This function determines the size of the input by counting the
+     * characters. Each character occupies 8 bits, including whitespace.
+     * Args: fileString, which is the contents of a text file as a string
+     * Returns: an integer representing the size of the input
+     */
+        return 0;
+    }
+
+    public int outputSize(Huffman<K,V> tree, String code) {
+    /**
+     * This function determines the size of the output by finding the size of
+     * the Huffman tree and the encoded sequence
+     * Args:
+     *      tree, which is a Huffman object
+     *      code, which is a string representing the encoded input data
+     */
+        return 0;
+    }
+
+    public String decode(Huffman<K,V> tree, String code) {
+    /**
+     * This function decodes the Huffman tree using the given code. It creates
+     * a string representing the final decoded value, which should match the
+     * input data
+     * Args: 
+     *      tree, which is a Huffman object
+     *      code, which is a string representing the encoded input data
+     */
+        return "";
+    }
+}
