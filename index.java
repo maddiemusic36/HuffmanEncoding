6b2a5925b8b2881295d169a67c298ffb9ec35f67
Madeline DeLeon <mdamusic36@gmail.com>
2023-11-06 15:37:35 -0700

    housekeeping

--- index.java
+++ index.java
@@ -10,14 +10,14 @@ import java.util.Scanner;
 
 public class index<K, V> {
 
-    public PriorityQueue organizeInput(String filename) throws FileNotFoundException {
+    public PriorityQueue organizeInput(String filename, String content) throws FileNotFoundException {
     /**
      * This function reads in the text from the given file, counts the
      * frequency of each character, and creates a priority queue of said
-     * frequencies.
+     * characters and frequencies.
      * Args: filename, which is a string representing the name of a file
      * Returns: a PriorityQueue object which holds every character and their
-     *              frequencies from the given file
+     *          frequencies from the given file
      */
         // priority queue to hold Nodes
         PriorityQueue pQueue = new PriorityQueue();
@@ -30,21 +30,21 @@ public class index<K, V> {
         // loop through every line in the file
         while (myReader.hasNextLine()) {
             String data = myReader.nextLine();
+            content += data;
             // loop through every character in the current line
             for (int i = 0; i < data.length(); i++){
                 char letter = data.charAt(i);
                 Node maybe = pQueue.find(letter);
-                // check if the letter already exists in the queue
+                // check if the character already exists in the queue
                 if (maybe == null)
                     pQueue.enqueue(new Node(letter, 0));
                 else { maybe.frequency++;}
             }
         }
 
+        // close the file
         myReader.close();
-
         return pQueue;
-
     }
 
     public int inputSize(String fileString) {
@@ -69,6 +69,7 @@ public class index<K, V> {
      *      tree, which is a Huffman object
      *      code, which is a string representing the encoded input data
      */
+        //////////
         return 0;
     }
 
@@ -81,15 +82,37 @@ public class index<K, V> {
      *      tree, which is a Huffman object
      *      code, which is a string representing the encoded input data
      */
+        /////////
         return "";
     }
 
-    public static void test() {
-        // read the input file
-        // determine the size of the input
-        // convert the data of the input into a priority queue
-        // create the huffman tree
-        // create the encoding
-        // determine the size of the encoding and the tree
+    public void test(String filename) {
+    /**
+     * This method is used for testing our implementation. It runs through the
+     * entire process and displays any relevant information.
+     * Args: filename, which is the name of the file containing the input data
+     * Returns: None
+     */
+        try {
+            String fileString = "";
+
+            // read the input file and convert the data into a priority queue
+            PriorityQueue pQueue = organizeInput(filename, fileString);
+
+            // determine the size of the input
+            int inputSize = inputSize(fileString);
+
+            // create the huffman tree
+            /////////
+
+            // create the encoding
+            /////////
+
+            // determine the size of the encoding and the tree
+            //////////
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
